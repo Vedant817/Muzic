3bafb77671362e1761bea1bc7df119e60801a910
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-09-03 23:09:35 +0530

    Shadcn Installation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,6 @@
-export const YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
